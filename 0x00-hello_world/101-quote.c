bb40b433af65a6ba66b1ed6b149565a4930281e3
kingmarshal1 <kingmarshal123@gmail.com>
2023-02-16 13:56:34 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - prints a string and strandard error
+ * Returns:1
+ */
+int main(void)
+{
+write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
+return (1);
+}
+
