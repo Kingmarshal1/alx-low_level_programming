8245f0a3a79fbd777416b2ff6c8f7fb8da77736c
kingmarshal1 <kingmarshal123@gmail.com>
2023-02-17 16:16:58 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdion.h>
+
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[1]);
+	}
+	putchar('\n');
+	return (0);
+}
