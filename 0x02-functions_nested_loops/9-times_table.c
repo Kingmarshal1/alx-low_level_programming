fda3ac951e4ea269c16244f962e4d81fd0401b32
kingmarshal1 <kingmarshal123@gmail.com>
2023-02-21 16:33:48 -0800

    10-main.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,38 @@
 #include "main.h"
 
+/
+ * times_table - Prints the 9 times table
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+  int a, b, op;
 
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    times_table();
-	    return (0);
-	}
+  for (a = 0; a <= 9; a++)
+  {
+
+    _putchar(48);
+    for (b = 1; b <= 9; b++)
+    {
+    _putchar(48);
+    for (b = 1; b <= 9; b++)
+    {
+      op = a * b;
+      _putchar(44);
+      _putchar(32);
+      if (op <= 9)
+      {
+        _putchar(32);
+        _putchar(op + 48);
+      }
+      else
+      {
+        _putchar((op / 10) + 48);
+        _putchar((op % 10) + 48);
+      }
+    }
+    _putchar('\n');
+  }
+}
+
