deb0515b440122c7116cf00dce13591908cfa719
kingmarshal1 <kingmarshal123@gmail.com>
2023-03-20 12:18:20 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - prints the name of the file
- * Created by Chukwudike Igwe
- * Return: Always 0 (Success)
- */
-int ain(void)
+*main - prints the name of the file
+*
+*Return: always 0
+*/
+
+
+int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
